32c37474b3fe32b25d0b031fdcb72c154d64f2a4
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-01 19:37:47 +0700

    Method for handling post album data

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -1,6 +1,25 @@
 class AlbumsHandler {
-  constructor() {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+  }
 
+  async postAlbumHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload);
+
+    const { name, year } = request.payload;
+
+    const albumId = await this._service.addAlbum({ name, year });
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        albumId,
+      }
+    });
+    response.code(201);
+
+    return response;
   }
 }
 
