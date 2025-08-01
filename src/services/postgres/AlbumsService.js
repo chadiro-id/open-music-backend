776a066cab474b4755bffbf25638e9935cce9894
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-01 18:15:06 +0700

    Method for adding album data

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -1,6 +1,27 @@
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
 class AlbumsService {
   constructor() {
+    this._pool = new Pool();
+  }
 
+  async addAlbum({ name, year }) {
+    const id = nanoid(16);
+
+    const query = {
+      text: 'INSERT INTO albums VALUES($1, $2, $3) RETURNING id',
+      value: [id, name, year],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows[0].id) {
+      throw new InvariantError('Album gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
   }
 }
 
