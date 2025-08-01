d9ab4e525bb87ff5e20955974d0a80dcd5b7ddf2
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-01 20:29:02 +0700

    Registering albums plugin into Hapi server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const AlbumsService = require('./services/postgres/AlbumsService');
+const Validator = require('./validator');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -12,6 +17,16 @@ const init = async () => {
       },
     },
   });
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: Validator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
