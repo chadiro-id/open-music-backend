1317039518e475d148f7cb4b9517540d42e3d230
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-01 20:53:27 +0700

    Error handling on server pre response ext

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const Hapi = require('@hapi/hapi');
 const albums = require('./api/albums');
 const AlbumsService = require('./services/postgres/AlbumsService');
 const Validator = require('./validator');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const albumsService = new AlbumsService();
@@ -27,6 +28,22 @@ const init = async () => {
       },
     },
   ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    h.continue;
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
