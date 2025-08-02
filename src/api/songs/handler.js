fdfda6895c77e96be50e4085ca11961d4d4cb7dc
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-02 14:34:32 +0700

    New: songs api scripts

---
+++ src/api/songs/handler.js
@@ -0,0 +1,12 @@
+const autoBind = require('auto-bind');
+
+class SongsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+}
+
+module.exports = SongsHandler;
---
+++ src/api/songs/index.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  name: 'songs',
+  version: '1.0.0',
+  register: async () => {},
+};
