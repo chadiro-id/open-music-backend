aae0c7f6e44aae91a7cd70a42816f8433ff6edb9
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-02 14:45:53 +0700

    Method for handling post song data

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -7,6 +7,38 @@ class SongsHandler {
 
     autoBind(this);
   }
+
+  async postSongHandler(request, h) {
+    this._validator.validateSongPayload(request.payload);
+
+    const {
+      title,
+      year,
+      genre,
+      performer,
+      duration,
+      albumId
+    } = request.payload;
+
+    const songId = await this._service.addSong({
+      title,
+      year,
+      genre,
+      performer,
+      duration,
+      albumId
+    });
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        songId,
+      }
+    });
+    response.code(201);
+
+    return response;
+  }
 }
 
 module.exports = SongsHandler;
