f7f9b05c9d5c036a2a245adb092efbf17b2ab609
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-02 13:52:43 +0700

    Method for getting song by given id

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -36,6 +36,20 @@ class SongsService {
     const result = await this._pool.query('SELECT * FROM songs');
     return result.rows.map(mapSongDataToModel);
   }
+
+  async getSongById(id) {
+    const query = {
+      text: 'SELECT * FROM songs WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new NotFoundError('Lagu tidak ditemukan');
+    }
+
+    return result.rows.map(mapSongDataToModel)[0];
+  }
 }
 
 module.exports = SongsService;
