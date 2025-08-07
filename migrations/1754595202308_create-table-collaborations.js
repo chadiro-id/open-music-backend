1e5baa9406292e93d0e4e14bb72bf2cf0784ed72
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-08 02:40:46 +0700

    Migrations strategy for users playlist collaborations table

---
+++ migrations/1754595202308_create-table-collaborations.js
@@ -0,0 +1,55 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('collaborations', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    }
+  });
+
+  pgm.addConstraint('collaborations', 'collaborations_playlist_id_user_id_key', {
+    unique: [
+      'playlist_id',
+      'user_id'
+    ],
+  });
+
+  pgm.addConstraint('collaborations', 'collaborations_playlist_id_fkey', {
+    foreignKeys: {
+      columns: 'playlist_id',
+      references: 'playlists(id)',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+  });
+
+  pgm.addConstraint('collaborations', 'collaborations_user_id_fkey', {
+    foreignKeys: {
+      columns: 'user_id',
+      references: 'users(id)',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('collaborations');
+};
