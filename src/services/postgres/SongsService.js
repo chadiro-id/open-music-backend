0de8354576a6ae26402bdad751dc1fe8f034aa34
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-08 11:04:45 +0700

    Reorganizing node postgres client config file

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -1,4 +1,4 @@
-const db = require('../../db');
+const db = require('../../db/postgres');
 const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
