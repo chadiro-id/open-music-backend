8263362b05bc10127e3477c9632b06c28a3d09d2
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-08 20:29:34 +0700

    Method for adding new refresh token data

--- src/services/postgres/AuthenticationsService.js
+++ src/services/postgres/AuthenticationsService.js
@@ -2,7 +2,14 @@ const db = require('../../db/postgres');
 const InvariantError = require('../../exceptions/InvariantError');
 
 class AuthenticationsService {
+  async addRefreshToken(token) {
+    const query = {
+      text: 'INSERT INTO authentications VALUES($1)',
+      values: [token],
+    };
 
+    await this._pool.query(query);
+  }
 }
 
 module.exports = AuthenticationsService;
