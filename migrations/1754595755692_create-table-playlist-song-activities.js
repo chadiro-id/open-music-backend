9b75ddb356c08511167a7f13e8acac5e2684e5d9
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-08 10:19:41 +0700

    Migrations strategy for playlist song activities table

---
+++ migrations/1754595755692_create-table-playlist-song-activities.js
@@ -0,0 +1,44 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createType('playlist_song_action', ['add', 'delete']);
+
+  pgm.createTable('playlist_song_activities', {
+    id: {
+      type:'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    action: {
+      type: 'playlist_song_action',
+      notNull: true,
+    },
+    time: {
+      type: 'TIMESTAMPTZ',
+    }
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('playlist_song_activities');
+  pgm.dropType('playlist_song_action');
+};
