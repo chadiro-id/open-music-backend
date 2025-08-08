e103396a2e3337d00594ec54f68a96fae29d31ea
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-08 20:26:50 +0700

    New: authentications service scripts

---
+++ src/services/postgres/AuthenticationsService.js
@@ -0,0 +1,8 @@
+const db = require('../../db/postgres');
+const InvariantError = require('../../exceptions/InvariantError');
+
+class AuthenticationsService {
+
+}
+
+module.exports = AuthenticationsService;
