089cfdecc4541fee6052ff60df53d4b2f37d212e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-08 18:06:15 +0700

    Method for verifying new username availability

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,4 +1,19 @@
+const db = require('../../db/postgres');
+const InvariantError = require('../../exceptions/InvariantError');
+
 class UsersService {
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await db.query(query);
+    if (result.rowCount) {
+      throw new InvariantError('Gagal menambahkan user. Username sudah digunakan');
+    }
+  }
 
 }
 
