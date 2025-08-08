6efa59e83169e69cd9e192d3b51689b0feeb5330
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-08 18:30:08 +0700

    Method for adding new user data

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,7 +1,27 @@
 const db = require('../../db/postgres');
+const bcrypt = require('bcrypt');
+const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
 
 class UsersService {
+  async addUser({ username, password, fullname }) {
+    await this.verifyNewUsername(username);
+
+    const id = nanoid(16);
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const query = {
+      text: 'INSERT INTO users VALUES (CONCAT(\'user-\', $1::text, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
+    };
+
+    const result = await db.query(query);
+    if (!result.rowCount) {
+      throw new InvariantError('User gagal ditambahkan');
+    }
+
+    return result.rows[0]?.id;
+  }
 
   async verifyNewUsername(username) {
     const query = {
@@ -14,7 +34,6 @@ class UsersService {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan');
     }
   }
-
 }
 
 module.exports = UsersService;
