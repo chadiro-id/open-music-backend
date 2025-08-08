fca8efe12b6ebff3bed7b1ab75ddd42c1f700fa6
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-08 18:36:25 +0700

    Method for getting user data by the given id

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -2,6 +2,7 @@ const db = require('../../db/postgres');
 const bcrypt = require('bcrypt');
 const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class UsersService {
   async addUser({ username, password, fullname }) {
@@ -33,6 +34,20 @@ class UsersService {
     if (result.rowCount) {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan');
     }
+  }
+
+  async getUserById(userId) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE id = $1',
+      values: [userId],
+    };
+
+    const result = await db.query(query);
+    if (!result.rowCount) {
+      throw new NotFoundError('User tidak ditemukan');
+    }
+
+    return result.rows[0];
   }
 }
 
