5287bacadc7ceac6ff67a422df871e1cf386c536
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-10 01:09:54 +0700

    Configure dependencies

--- src/api/collaborations/handler.js
+++ src/api/collaborations/handler.js
@@ -1,5 +1,16 @@
-class CollborationsHandler {
+const autoBind = require('auto-bind');
 
+class CollborationsHandler {
+  constructor(services, validator) {
+    [
+      this._collaborationsService,
+      this._playlistsService
+    ] = services;
+
+    this._validator = validator;
+
+    autoBind(this);
+  }
 }
 
 module.exports = CollborationsHandler;
