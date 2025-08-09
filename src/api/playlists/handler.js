38a990ef15522fc22019192816b2929ec91365af
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-09 13:01:17 +0700

    Method for handling post request playlist

--- src/api/playlists/handler.js
+++ src/api/playlists/handler.js
@@ -7,6 +7,26 @@ class PlaylistsHandler {
 
     autoBind(this);
   }
+
+  async postPlaylistHandler(request, h) {
+    this._validator.validatePlaylistPayload(request.payload);
+
+    const { name } = request.payload;
+    const { id: credentialId } = request.auth.credentials;
+
+    const playlistId = await this._service.addPlaylist({ name, owner: credentialId });
+
+    const response = h.response({
+      status: 'success',
+      message: 'Playlist berhasil ditambahkan',
+      data: {
+        playlistId,
+      },
+    });
+
+    response.code(201);
+    return response;
+  }
 }
 
 module.exports = PlaylistsHandler;
