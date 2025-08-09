eab170d55a2ec36aa12eb416fe52e89c418b7d4a
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-10 01:44:07 +0700

    Defining collaborations routes

--- src/api/collaborations/routes.js
+++ src/api/collaborations/routes.js
@@ -1,3 +1,20 @@
-const routes = () => [];
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/collaborations',
+    handler: handler.postCollaborationHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/collaborations',
+    handler: handler.deleteCollaborationHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+];
 
 module.exports = routes;
