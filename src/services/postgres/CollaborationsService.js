2857e7cfd481b06897bbd2e695937124f8b72e5f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-10 00:34:43 +0700

    Method for adding new collaboration data

--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -1,5 +1,23 @@
-class CollaborationsService {
+const db = require('../../db/postgres');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
 
+class CollaborationsService {
+  async addCollaboration({ playlistId, userId }) {
+    const id = `collab-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO collaborations VALUES($1, $2, $3) RETURNING id',
+      values: [id, playlistId, userId],
+    };
+
+    const result = await db.query(query);
+
+    if (!result.rows[0]?.id) {
+      throw new InvariantError('Kolaborasi gagal ditambahkan');
+    }
+    return result.rows[0].id;
+  }
 }
 
 module.exports = CollaborationsService;
