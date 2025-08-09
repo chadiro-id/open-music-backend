d77ae662b7f44ffdb530cf8d623fe26252e45e7c
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-09 12:33:00 +0700

    Configure dependencies

--- src/api/playlists/handler.js
+++ src/api/playlists/handler.js
@@ -1,5 +1,12 @@
-class PlaylistsHandler {
+const autoBind = require('auto-bind');
 
+class PlaylistsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
 }
 
 module.exports = PlaylistsHandler;
