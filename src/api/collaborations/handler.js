d8a33b6fa3cbbda53d3e5c3a88ac8b284c06200d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-10 01:37:57 +0700

    Method for handling post request collaboration data

--- src/api/collaborations/handler.js
+++ src/api/collaborations/handler.js
@@ -11,6 +11,28 @@ class CollborationsHandler {
 
     autoBind(this);
   }
+
+  async postCollaborationHandler(request, h) {
+    this._validator.validateCollaborationPayload(request.payload);
+
+    const { id: credentialId } = request.auth.credentials;
+    const { playlistId, userId } = request.payload;
+
+    await this._playlistsService.verifyPlaylistOwner(playlistId, credentialId);
+
+    const collaborationId = await this._collaborationsService.addCollaboration({ playlistId, userId });
+
+    const response = h.response({
+      status: 'success',
+      message: 'Kolaborasi berhasil ditambahkan',
+      data: {
+        collaborationId,
+      },
+    });
+
+    response.code(201);
+    return response;
+  }
 }
 
 module.exports = CollborationsHandler;
