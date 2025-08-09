5bbff4ee737ed2a0e9e83f136910d8644251a811
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-09 17:34:33 +0700

    Add playlist song payload validation

--- src/validator/playlists/index.js
+++ src/validator/playlists/index.js
@@ -1,9 +1,19 @@
 const InvariantError = require('../../exceptions/InvariantError');
-const { PlaylistPayloadSchema } = require('./schema');
+const {
+  PlaylistPayloadSchema,
+  PlaylistSongPayloadSchema
+} = require('./schema');
 
 const PlaylistsValidator = {
   validatePlaylistPayload: (payload) => {
     const result = PlaylistPayloadSchema.validate(payload);
+
+    if (result.error) {
+      throw new InvariantError(result.error.message);
+    }
+  },
+  validatePlaylistSongPayload: (payload) => {
+    const result = PlaylistSongPayloadSchema.validate(payload);
 
     if (result.error) {
       throw new InvariantError(result.error.message);
--- src/validator/playlists/schema.js
+++ src/validator/playlists/schema.js
@@ -4,6 +4,11 @@ const PlaylistPayloadSchema = Joi.object({
   name: Joi.string().required(),
 });
 
+const PlaylistSongPayloadSchema = Joi.object({
+  songId: Joi.string().required(),
+});
+
 module.exports = {
-  PlaylistPayloadSchema
+  PlaylistPayloadSchema,
+  PlaylistSongPayloadSchema
 };
