315094ff11b5593635812cb693d246ba07ec68a8
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-09 13:59:07 +0700

    Defining plugin register value

--- src/api/playlists/index.js
+++ src/api/playlists/index.js
@@ -1,7 +1,13 @@
+const PlaylistsHandler = require('./handler');
+const routes = require('./routes');
+
 const plugin = {
   name: 'playlists',
   version: '1.0.0',
-  register: () => {},
+  register: async (server, { service, validator }) => {
+    const handler = new PlaylistsHandler(service, validator);
+    server.route(routes(handler));
+  },
 };
 
 module.exports = plugin;
