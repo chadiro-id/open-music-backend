e0a21ca763629b8382f9ef693a64d5500c7286f9
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-10 01:49:29 +0700

    Configure plugin register prop

--- src/api/collaborations/index.js
+++ src/api/collaborations/index.js
@@ -1,7 +1,14 @@
+const CollaborationsHandler = require('./handler');
+const routes = require('./routes');
+
 const plugin = {
   name: 'collaborations',
   version: '1.0.0',
-  register: async () => {},
+  register: async (server, { services, validator }) => {
+    const handler = new CollaborationsHandler(services, validator);
+
+    server.route(routes(handler));
+  },
 };
 
 module.exports = plugin;
