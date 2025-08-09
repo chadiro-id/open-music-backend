8da578edf0974dfaa0a1c29ff3065e4ff6ff1463
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-09 14:04:15 +0700

    Registering playlists api plugin

--- src/server.js
+++ src/server.js
@@ -22,11 +22,16 @@ const AuthenticationsService = require('./services/postgres/AuthenticationsServi
 const TokenManager = require('./tokenize/TokenManager');
 const AuthenticationsValidator = require('./validator/authentications');
 
+const playlists = require('./api/playlists');
+const PlaylistsService = require('./services/postgres/PlaylistsService');
+const PlaylistsValidator = require('./validator/playlists');
+
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const playlistsService = new PlaylistsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -91,6 +96,13 @@ const init = async () => {
         validator: AuthenticationsValidator,
       },
     },
+    {
+      plugin: playlists,
+      options: {
+        service: playlistsService,
+        validator: PlaylistsValidator,
+      }
+    }
   ]);
 
   server.ext('onPreResponse', (request, h) => {
