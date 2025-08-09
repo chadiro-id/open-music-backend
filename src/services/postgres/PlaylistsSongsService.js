83247eadc1be5bf820a6d05810b6be7aac958f54
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-09 14:52:54 +0700

    Method for adding new playlist song data

--- src/services/postgres/PlaylistsSongsService.js
+++ src/services/postgres/PlaylistsSongsService.js
@@ -1,5 +1,23 @@
-class PlaylistsSongsService {
+const db = require('../../db/postgres');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
 
+class PlaylistsSongsService {
+  async addPlaylistSong({ playlistId, songId }) {
+    const id = `ps-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO playlists_songs VALUES ($1, $2, $3) RETURNING id',
+      values: [id, playlistId, songId],
+    };
+
+    const result = await db.query(query);
+    if (!result.rows[0]?.id) {
+      throw new InvariantError('Playlist song gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
 }
 
 module.exports = PlaylistsSongsService;
