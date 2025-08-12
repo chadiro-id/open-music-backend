daf1e1e00df1972b41229976a6dfc45dfd6af5fb
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-12 10:24:20 +0700

    Minor changes for code consistency and readability

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -10,8 +10,8 @@ class UsersHandler {
 
   async postUserHandler(request, h) {
     this._validator.validateUserPayload(request.payload);
-    const { username, password, fullname } = request.payload;
 
+    const { username, password, fullname } = request.payload;
     const userId = await this._service.addUser({ username, password, fullname });
 
     const response = h.response({
@@ -21,6 +21,7 @@ class UsersHandler {
         userId,
       },
     });
+
     response.code(201);
     return response;
   }
