271796b241a02b1050acee57037592552ddb60d0
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-12 10:18:08 +0700

    Minor changes for code readability

--- src/api/playlists/index.js
+++ src/api/playlists/index.js
@@ -4,8 +4,18 @@ const routes = require('./routes');
 const plugin = {
   name: 'playlists',
   version: '1.0.0',
-  register: async (server, { playlistsService, songsService, playlistSongsService, validator }) => {
-    const handler = new PlaylistsHandler(playlistsService, songsService, playlistSongsService, validator);
+  register: async (server, {
+    playlistsService,
+    songsService,
+    playlistSongsService,
+    validator
+  }) => {
+    const handler = new PlaylistsHandler(
+      playlistsService,
+      songsService,
+      playlistSongsService,
+      validator
+    );
     server.route(routes(handler));
   },
 };
