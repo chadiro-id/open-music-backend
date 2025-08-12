1fbedd76edf61a0892436bd7b8498a8f0ed58a51
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-12 09:46:10 +0700

    Giving prefix for id directly in code rather than in query

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -5,10 +5,10 @@ const NotFoundError = require('../../exceptions/NotFoundError');
 
 class AlbumsService {
   async addAlbum({ name, year }) {
-    const id = nanoid(16);
+    const id = `album-${nanoid(16)}`;
 
     const query = {
-      text: 'INSERT INTO albums VALUES(CONCAT(\'album-\', $1::text), $2, $3) RETURNING id',
+      text: 'INSERT INTO albums VALUES($1, $2, $3) RETURNING id',
       values: [id, name, year],
     };
 
