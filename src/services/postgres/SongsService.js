db7f434ee4ebeb7b57dff1a919aa1db1f2a83277
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-12 10:02:32 +0700

    Giving prefix for id directly in code rather than in query

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -13,10 +13,10 @@ class SongsService {
     duration,
     albumId
   }) {
-    const id = nanoid(16);
+    const id = `song-${nanoid(16)}`;
 
     const query = {
-      text: 'INSERT INTO songs VALUES(CONCAT(\'song-\', $1::text), $2, $3, $4, $5, $6, $7) RETURNING id',
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id',
       values: [id, title, year, genre, performer, duration, albumId]
     };
 
