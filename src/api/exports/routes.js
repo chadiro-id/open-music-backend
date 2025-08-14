b3008a1811d6cf19e5fd79a680c9e5a717ee5d8e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-14 18:01:56 +0700

    Configure route for exporting playlist

--- src/api/exports/routes.js
+++ src/api/exports/routes.js
@@ -1,3 +1,12 @@
-const routes = () => [];
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: 'export/playlists/{playlistId}',
+    handler: handler.postExportPlaylistsHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
+  },
+];
 
 module.exports = routes;
