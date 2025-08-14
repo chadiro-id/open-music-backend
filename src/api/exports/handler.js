b83c8dad51949cfd227388d781c6e3c2def80160
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-14 17:51:58 +0700

    Method for handling post request for exporting playlists

--- src/api/exports/handler.js
+++ src/api/exports/handler.js
@@ -7,6 +7,25 @@ class ExportsHandler {
 
     autoBind(this);
   }
+
+  async postExportPlaylistsHandler(request, h) {
+    this._validator.validateExportPlaylistsPayload(request.payload);
+
+    const message = {
+      userId: request.auth.credentials.id,
+      targetEmail: request.payload.targetEmail,
+    };
+
+    await this._service.sendMessage('export:playlists', JSON.stringify(message));
+
+    const response = h.response({
+      status: 'success',
+      message: 'Permintaan Anda sedang kami proses',
+    });
+
+    response.code(201);
+    return response;
+  }
 }
 
 module.exports = ExportsHandler;
