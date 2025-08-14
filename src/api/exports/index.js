11cfa4242347454566dfcd117d452245325d0502
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-14 18:38:35 +0700

    Changing dependencies for handler instance

--- src/api/exports/index.js
+++ src/api/exports/index.js
@@ -4,8 +4,16 @@ const routes = require('./routes');
 module.exports = {
   name: 'exports',
   version: '1.0.0',
-  register: async (server, { service, validator }) => {
-    const handler = new ExportsHandler(service, validator);
+  register: async (server, {
+    playlistsService,
+    producerService,
+    validator
+  }) => {
+    const handler = new ExportsHandler(
+      playlistsService,
+      producerService,
+      validator
+    );
     server.route(routes(handler));
   },
 };
