cf0200c3b1d1271c78bffbd0b55937b60fdebba4
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-14 18:08:41 +0700

    Configure register prop of plugin

--- src/api/exports/index.js
+++ src/api/exports/index.js
@@ -1,5 +1,11 @@
+const ExportsHandler = require('./handler');
+const routes = require('./routes');
+
 module.exports = {
   name: 'exports',
   version: '1.0.0',
-  register: async () => {},
+  register: async (server, { service, validator }) => {
+    const handler = new ExportsHandler(service, validator);
+    server.route(routes(handler));
+  },
 };
