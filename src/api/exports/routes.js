49cf99474fa21f16b67bb7711b64dcf5b52d83d9
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-14 18:17:01 +0700

    Fix: route path

--- src/api/exports/routes.js
+++ src/api/exports/routes.js
@@ -1,7 +1,7 @@
 const routes = (handler) => [
   {
     method: 'POST',
-    path: 'export/playlists/{playlistId}',
+    path: '/export/playlists/{playlistId}',
     handler: handler.postExportPlaylistsHandler,
     options: {
       auth: 'openmusic_jwt',
