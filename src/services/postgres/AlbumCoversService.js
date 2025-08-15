3d2596b322e343dd6e1baff5c859bdcbe744bc02
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-15 18:45:37 +0700

    Method for adding new album cover

--- src/services/postgres/AlbumCoversService.js
+++ src/services/postgres/AlbumCoversService.js
@@ -1,6 +1,19 @@
+const db = require('../../db/postgres');
+const InvariantError = require('../../exceptions/InvariantError');
+
 class AlbumCoversService {
   async addAlbumCover({ albumId, cover }) {
-    console.log(albumId, cover);
+    const query = {
+      text: 'INSERT INTO album_covers (album_id, cover) VALUES ($1, $2) RETURNING id',
+      values: [albumId, cover],
+    };
+
+    const result = await db.query(query);
+    if (!result.rows[0]?.id) {
+      throw new InvariantError('Album gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
   }
 }
 
