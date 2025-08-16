613f2c6f9f9bc5784794288432fb872a00fa1090
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-16 15:58:40 +0700

    Configure logic for adding like to album

--- src/services/postgres/UserAlbumLikesService.js
+++ src/services/postgres/UserAlbumLikesService.js
@@ -1,6 +1,17 @@
+const db = require('../../db/postgres');
+const InvariantError = require('../../exceptions/InvariantError');
+
 class UserAlbumLikesService {
   async addLikeToAlbum({ userId, albumId }) {
-    console.log(userId, albumId);
+    const query = {
+      text: 'INSERT INTO user_album_likes (user_id, album_id) VALUES ($1, $2) RETURNING id',
+      values: [userId, albumId],
+    };
+
+    const result = await db.query(query);
+    if (!result.rowCount) {
+      throw new InvariantError('Like gagal ditambahkan');
+    }
   }
 
   async getLikesFromAlbum(albumId) {
