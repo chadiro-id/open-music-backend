ce47a2e8c1615b267e160fa73dac0e0ada5f5ff6
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-16 13:46:08 +0700

    Configure method for upload and delete files

--- src/services/S3/StorageService.js
+++ src/services/S3/StorageService.js
@@ -1,4 +1,10 @@
-const { S3Client, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand,
+} = require('@aws-sdk/client-s3');
+
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 
 class StorageService {
@@ -12,25 +18,29 @@ class StorageService {
     });
   }
 
-  async writeFile(file, meta) {
-    const parameter = new PutObjectCommand({
+  async uploadFile(key, fileBuffer, mimeType) {
+    const command = new PutObjectCommand({
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: meta.filename,
-      Body: file._data,
-      ContentType: meta.headers['content-type'],
+      Key: key,
+      Body: fileBuffer,
+      ContentType: mimeType,
     });
 
-    await this._client.send(parameter);
-
-    return this.createPreSignedUrl({
-      bucket: process.env.AWS_BUCKET_NAME,
-      key: meta.filename,
-    });
+    await this._client.send(command);
   }
 
-  createPreSignedUrl({ bucket, key }) {
-    const command = new GetObjectCommand({ Bucket: bucket, Key: key });
-    return getSignedUrl(this._client, command, { expiresIn: 3600 });
+  async deleteFile(key) {
+    const command = new DeleteObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: key,
+    });
+
+    await this._client.send(command);
+  }
+
+  generateSignedUrl(key, expiresIn = 3600) {
+    const command = new GetObjectCommand({ Bucket: process.env.AWS_BUCKET_NAME, Key: key });
+    return getSignedUrl(this._client, command, { expiresIn });
   }
 }
 
