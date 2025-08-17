2c249bc06923fd80e8f279043ce2c06500233f8e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-17 23:24:49 +0700

    Migrations for creating album likes table

---
+++ migrations/1755447168602_create-table-album-likes.js
@@ -0,0 +1,53 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('album_likes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    }
+  });
+
+  pgm.addConstraint('album_likes', 'album_likes_album_id_fkey', {
+    foreignKeys: {
+      columns: 'album_id',
+      references: 'albums(id)',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    }
+  });
+
+  pgm.addConstraint('album_likes', 'album_likes_user_id_fkey', {
+    foreignKeys: {
+      columns: 'user_id',
+      references: 'users(id)',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    }
+  });
+
+  pgm.addConstraint('album_likes', 'album_likes_album_id_user_id_key', { unique: ['album_id', 'user_id'] });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropConstraint('album_likes', 'album_likes_album_id_user_id_key');
+  pgm.dropConstraint('album_likes', 'album_likes_user_id_fkey');
+  pgm.dropConstraint('album_likes', 'album_likes_album_id_fkey');
+  pgm.dropTable('album_likes');
+};
