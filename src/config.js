9dd34a349d33f0b1697e437bfd04dc57bd7d6e0e
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-17 13:49:12 +0700

    New: configs file

---
+++ src/config.js
@@ -0,0 +1,27 @@
+const config = {
+  app: {
+    host: process.env.HOST,
+    port: process.env.PORT,
+  },
+  tokenize: {
+    accessTokenKey: process.env.ACCESS_TOKEN_KEY,
+    refreshTokenKey: process.env.REFRESH_TOKEN_KEY,
+    accessTokenAge: process.env.ACCESS_TOKEN_AGE,
+  },
+  aws: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    s3: {
+      region: process.env.AWS_REGION,
+      bucketName: process.env.AWS_BUCKET_NAME,
+    },
+  },
+  rabbitMq: {
+    server: process.env.RABBITMQ_SERVER,
+  },
+  redis: {
+    host: process.env.REDIS_SERVER,
+  },
+};
+
+module.exports = config;
