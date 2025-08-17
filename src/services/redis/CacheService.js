49ea6f009dda91efe5f2d500d2ab818aba50be6f
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-17 15:43:24 +0700

    Separating redis client

--- src/services/redis/CacheService.js
+++ src/services/redis/CacheService.js
@@ -1,40 +1,25 @@
-const config = require('../../config');
-const redis = require('redis');
+const client = require('../../infras/redis/client');
 
 class CacheService {
-  constructor() {
-    this._client = redis.createClient({
-      socket: {
-        host: config.redis.host,
-      },
-    });
-
-    this._client.on('error', (error) => {
-      console.error(error);
-    });
-
-    this._client.connect();
-  }
-
   async set(key, value, expirationInSecond = 1800) {
-    const result = await this._client.set(key, value, {
+    const result = await client.set(key, value, {
       EX: expirationInSecond,
     });
     console.log(`[Cache Service] set -> key: ${key}, val: ${value}, result: ${result}`);
-    const exists = await this._client.exists(key);
+    const exists = await client.exists(key);
     console.log(`[Cache Service] set -> key: ${key}, exists: ${exists}`);
   }
 
   async get(key) {
-    const result = await this._client.get(key);
+    const result = await client.get(key);
     console.log(`[Cache Service] get -> key: ${key}, result: ${result}`);
     return result;
   }
 
   async delete(key) {
-    const exists = await this._client.exists(key);
+    const exists = await client.exists(key);
     console.log(`[Cache Service] del -> key: ${key}, exists: ${exists}`);
-    const result = await this._client.del(key);
+    const result = await client.del(key);
     console.log(`[Cache Service] del -> key: ${key}, result: ${result}`);
   }
 }
