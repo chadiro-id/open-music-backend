5c78fb715bc4bf784d1cc86a74cfd6d142b92f20
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-18 23:26:01 +0700

    Implement caching for refresh token

--- src/services/postgres/AuthenticationsService.js
+++ src/services/postgres/AuthenticationsService.js
@@ -2,6 +2,10 @@ const db = require('../../infras/postgres');
 const InvariantError = require('../../exceptions/InvariantError');
 
 class AuthenticationsService {
+  constructor(cacheService) {
+    this._cacheService = cacheService;
+  }
+
   async addRefreshToken(token) {
     const query = {
       text: 'INSERT INTO authentications VALUES($1)',
@@ -9,9 +13,15 @@ class AuthenticationsService {
     };
 
     await db.query(query);
+    await this._cacheService.setRefreshToken(token);
   }
 
   async verifyRefreshToken(token) {
+    const cachedRefreshToken = await this._cacheService.verifyRefreshToken(token);
+    if (cachedRefreshToken) {
+      return;
+    }
+
     const query = {
       text: 'SELECT token FROM authentications WHERE token = $1',
       values: [token],
@@ -19,7 +29,7 @@ class AuthenticationsService {
 
     const result = await db.query(query);
 
-    if (!result.rows.length) {
+    if (!result.rowCount) {
       throw new InvariantError('Refresh token tidak valid');
     }
   }
@@ -31,6 +41,7 @@ class AuthenticationsService {
     };
 
     await db.query(query);
+    await this._cacheService.deleteRefreshToken(token);
   }
 }
 
