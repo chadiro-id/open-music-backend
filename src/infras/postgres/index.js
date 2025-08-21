002e999218e48263e5a93fc2e90bee77cc21b1ba
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-22 01:14:50 +0700

    Node postgres configurations

---
+++ src/infras/postgres/index.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+
+const pool = new Pool();
+
+module.exports = {
+  query: (text, params) => {
+    return pool.query(text, params);
+  },
+  getClient: () => {
+    return pool.connect();
+  }
+};
