047da96d6d19a6c60ccce807582a0db2ea3cbad9
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-22 10:22:55 +0700

    Adding methods for managing album cache

--- src/services/redis/AlbumsCacheService.js
+++ src/services/redis/AlbumsCacheService.js
@@ -1,6 +1,68 @@
-class AlbumsCacheService {
-  constructor() {
+const { client } = require('../../infras/redis/client');
 
+class AlbumsCacheService {
+  async setAlbum(id, value, expirationInSecond = 1800) {
+    const key = `albums:${id}`;
+    await client.set(key, value, { EX: expirationInSecond });
+  }
+
+  async getAlbum(id) {
+    const key = `albums:${id}`;
+    const result = await client.get(key);
+    return result;
+  }
+
+  async deleteAlbum(id) {
+    const key = `albums:${id}`;
+    await client.del(key);
+  }
+
+  async setAlbumCoverUrl(id, value, expirationinSecond = 1800) {
+    const key = `albums:${id}:cover_url`;
+    await client.set(key, value, { EX: expirationinSecond });
+  }
+
+  async getAlbumCoverUrl(id) {
+    const key = `albums:${id}:cover_url`;
+    const result = await client.get(key);
+    return result;
+  }
+
+  async deleteAlbumCoverUrl(id) {
+    const key = `albums:${id}:cover_url`;
+    await client.del(key);
+  }
+
+  async setAlbumLikesCount(id, value, expirationInSecond = 1800) {
+    const key = `albums:${id}:likes_count`;
+    await client.set(key, value, { EX: expirationInSecond });
+  }
+
+  async getAlbumLikesCount(id) {
+    const key = `albums:${id}:likes_count`;
+    const result = await client.get(key);
+    return result;
+  }
+
+  async deleteAlbumLikesCount(id) {
+    const key = `albums:${id}:likes_count`;
+    await client.del(key);
+  }
+
+  async setAlbumSongs(id, value, expirationInSecond = 1800) {
+    const key = `albums:${id}:songs`;
+    await client.set(key, value, { EX: expirationInSecond });
+  }
+
+  async getAlbumSongs(id) {
+    const key = `albums:${id}:songs`;
+    const result = await client.get(key);
+    return result;
+  }
+
+  async deleteAlbumSongs(id) {
+    const key = `albums:${id}:songs`;
+    await client.del(key);
   }
 }
 
