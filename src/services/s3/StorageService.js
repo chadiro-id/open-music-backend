c6fcd3be0f8d5531d275a78018336eae1ec38c70
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-22 11:27:08 +0700

    New: service scripts for managing file contents

---
+++ src/services/s3/StorageService.js
@@ -0,0 +1,15 @@
+class StorageService {
+  async uploadAlbumCoverImage(albumId) {
+    console.log(albumId);
+  }
+
+  async deleteAlbumCoverImage(albumId) {
+    console.log(albumId);
+  }
+
+  async getAlbumCoverImageUrl(albumId) {
+    console.log(albumId);
+  }
+}
+
+module.exports = StorageService;
