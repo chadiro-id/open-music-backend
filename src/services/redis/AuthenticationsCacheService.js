4b54c10765d23a9f9c3081ce3c9745e49010d0ca
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-22 15:57:09 +0700

    Service scripts for managing auths cache

---
+++ src/services/redis/AuthenticationsCacheService.js
@@ -0,0 +1,21 @@
+const { client } = require('../../infras/redis/client');
+
+class AuthenticationsCacheService {
+  async setRefreshToken(token, expirationInSecond) {
+    const key = `refresh_token:${token}`;
+    await client.set(key, 1, { EX: expirationInSecond });
+  }
+
+  async deleteRefreshToken(token) {
+    const key = `refresh-token:${token}`;
+    await client.del(key);
+  }
+
+  async verifyRefreshToken(token) {
+    const key = `refresh-token:${token}`;
+    const result = client.exists(key);
+    return result;
+  }
+}
+
+module.exports = AuthenticationsCacheService;
