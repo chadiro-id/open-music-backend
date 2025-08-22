661b44ee8cbc663b2876dd78bf462998fe9d73e3
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-22 17:10:08 +0700

    Configure method for caching users playlists

--- src/services/redis/PlaylistsCacheService.js
+++ src/services/redis/PlaylistsCacheService.js
@@ -1,14 +1,31 @@
+const { client } = require('../../infras/redis/client');
+
 class PlaylistsCacheService {
   async addPlaylistsToUser(userId, values) {
-    console.log(userId, values);
+    const key = `playlists:${userId}`;
+
+    const multi = client.multi().rPush(key, ...values.map((val) => JSON.stringify(val)));
+
+    const ttl = await client.TTL(key);
+    if (ttl < 0) {
+      multi.expire(key, 1800);
+    }
+
+    const result = await multi.exec();
+    console.log(...result);
   }
 
-  async getPlaylistsByUser(userId) {
-    console.log(userId);
+  async getPlaylistsByUser(userId, start = 0, stop = -1) {
+    const key = `playlists:${userId}`;
+    const result = await client.lRange(key, start, stop);
+    console.log(result);
+    return result.map((element) => JSON.parse(element));
   }
 
   async deletePlaylistsForUser(userId) {
-    console.log(userId);
+    const key = `playlists:${userId}`;
+    const result = await this._client.del(key);
+    console.log(result);
   }
 
   async setPlaylist(playlistId, value) {
